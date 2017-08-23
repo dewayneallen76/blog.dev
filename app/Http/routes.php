8791d2ae4caf26a1043385f3c1e98ad76a7aa7db
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-23 14:12:16 -0500

    added routes exercises

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,18 @@ Route::get('/', function () {
 Route::get('/sayhello/{name}', function ($name) {
   return "Hey there, $name!";
 });
+
+// Create a route at the path /uppercase that takes a parameter that is a word and returns a string that is the word in all caps.
+Route::get('/uppercase/{string}', function($string) {
+  return "Uppercased: " .strtoupper($string);
+});
+
+// Create a route at the path /increment that takes a parameter that is a number and returns the number plus one.
+Route::get('/increment/{number?}', function ($number = 1) {
+  return $number + 1;
+});
+
+// Create a route at the path /add that takes two parametes that are numbers and returns the sum of the numbers.
+Route::get('/add/{a}/{b}', function ($a, $b) {
+  return $a + $b;
+});
