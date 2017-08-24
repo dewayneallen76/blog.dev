c4aaebda878e583de07a380b59537ce79433ae44
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-23 22:28:41 -0500

    added methods for increment and rolldice

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,4 +16,37 @@ class HomeController extends Controller
     return view('uppercase', $data);
   }
 
+  public function increment($number)
+  {
+    $message = '';
+    if(is_numeric($number)) {
+      $numberPlusOne = $number + 1;
+    } else {
+      $message = "Enter a number";
+    }
+    $data['message'] = $message;
+    $data['number'] = $number;
+    $data['numberPlusOne'] = $numberPlusOne;
+    return view('increment', $data);
+  }
+
+  public function rollDice($guess)
+  {
+    $dice = rand(1,6);
+
+    if(!is_numeric($guess)) {
+      $message = "Please enter a valid number!";
+    } else if($guess == $dice){
+      $message = "You guessed correctly! Well done!";
+    } else {
+      $message = "Try again!";
+    }
+
+    $data['guess'] = $guess;
+    $data['message'] = $message;
+    $data['dice'] = $dice;
+
+    return view('rolldice', $data);
+  }
+  
 }
