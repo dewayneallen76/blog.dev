5d72e3cb3568ae5bfeeb1bb10adbf4141f4d1dd2
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-23 22:30:21 -0500

    edited comments

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,12 +23,11 @@ Route::get('/uppercase/{word}', 'HomeController@upperCase');
 // Create a route at the path /increment that takes a parameter that is a number and returns the number plus one.
 Route::get('/increment/{number?}', 'HomeController@increment');
 
-// Create a route at the path /add that takes two parametes that are numbers and returns the sum of the numbers.
+// Create a route at the path /add that takes two parameters that are numbers and returns the sum of the numbers.
 Route::get('/add/{a}/{b}', function ($a, $b) {
   return $a + $b;
 });
 
 // Create a route that responds to a GET request on the path /rolldice.
 // Within the route, return a random number between 1 and 6.
-// Add a view named roll-dice.php. Instead of just returning the random number, show the view and have it display the random number.
 Route::get('/rolldice/{guess}', 'HomeController@rollDice');
