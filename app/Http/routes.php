b761d056e01db2aac800530b0e42a82d6f3f4329
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-23 22:29:23 -0500

    moved logic for increment and roll dice to methods created in HomeController. refactored routes to use HomeController.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,13 +21,7 @@ Route::get('/sayhello/{name}', function ($name) {
 Route::get('/uppercase/{word}', 'HomeController@upperCase');
 
 // Create a route at the path /increment that takes a parameter that is a number and returns the number plus one.
-Route::get('/increment/{number?}', function ($number = 1) {
-  if(is_numeric($number)) {
-    return $number + 1;
-  } else {
-    return 1;
-  }
-});
+Route::get('/increment/{number?}', 'HomeController@increment');
 
 // Create a route at the path /add that takes two parametes that are numbers and returns the sum of the numbers.
 Route::get('/add/{a}/{b}', function ($a, $b) {
@@ -37,21 +31,4 @@ Route::get('/add/{a}/{b}', function ($a, $b) {
 // Create a route that responds to a GET request on the path /rolldice.
 // Within the route, return a random number between 1 and 6.
 // Add a view named roll-dice.php. Instead of just returning the random number, show the view and have it display the random number.
-Route::get('/rolldice/{guess}', function ($guess) {
-
-  $dice = rand(1,6);
-
-  if(!is_numeric($guess)) {
-    $message = "Please enter a valid number!";
-  } else if($guess == $dice){
-    $message = "You guessed correctly! Well done!";
-  } else {
-    $message = "Try again!";
-  }
-
-  $data['guess'] = $guess;
-  $data['message'] = $message;
-  $data['dice'] = $dice;
-
-  return view('rolldice', $data);
-});
+Route::get('/rolldice/{guess}', 'HomeController@rollDice');
