b5b111e3c73bf3c96a89cef9a13530aea7be1e28
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-24 20:34:24 -0500

    added index blade that will show all posts

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title')
+  <title>All Posts</title>
+@stop
+
+@section('content')
+<div class="container">
+  <h1>All posts will be seen here</h1>
+</div>
+@stop
