e75ff6423be0d2e1b9faad82263bc484fd6590b1
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-25 11:15:18 -0500

    added form to create blade to get input for posts, added delete button with method spoofing with action to the delete method on the posts controller.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,7 +6,26 @@
 
 @section('content')
 <div class="container">
-  <h1>You will be able to create a post here.</h1>
+  <h2>Create a new post</h2>
+  <div class="form-group">
+    <form action="{{ action('PostsController@store')}}" method="POST">
+      {!! csrf_field() !!}
+      <label for="title">Title</label>
+      <input type="text" class="form-control" id="title" name="title" placeholder="{{ old('title') }}">
+      <label for="url">URL</label>
+      <input type="text" class="form-control" id="url" name="url" placeholder="{{ old('url') }}">
+      <label for="content">Content</label>
+      <textarea type="textarea" rows="5" class="form-control" id="content" name="content" placeholder="{{ old('content') }}"></textarea>
+      <br>
+      <button class="btn btn-primary btn-success">Create Post</button>
+    </form>
+    <br>
+    <form action="{{ action('PostsController@destroy') }}" method="post">
+      {!! csrf_field() !!}
+      <button class="btn btn-primary btn-danger" name="button">Delete Post</button>
+      {!! method_field('DELETE') !!}
+    </form>
+  </div>
 </div>
 
 @stop
