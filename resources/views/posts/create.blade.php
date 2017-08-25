367a76b73bef183e48dd91cfa88946affcfdc3bb
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-24 20:34:47 -0500

    created create blade that will be used to create a new post

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('title')
+  <title>Create A Post</title>
+@stop
+
+@section('content')
+<div class="container">
+  <h1>You will be able to create a post here.</h1>
+</div>
+
+@stop
