82821b76efb52c7d6166e485e3889bc87b816405
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-28 10:10:59 -0500

    added edit blade for posts

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,16 +4,27 @@
   <title>Edit Post</title>
 @stop
 
-
 @section('content')
-@extends('layouts.master')
-
-@section('title')
-  <title>Create A Post</title>
-@stop
-
-@section('content')
-  <h1>This will be where you edit</h1>
-@stop
-
+<div class="container">
+  <h2>Edit Post</h2>
+  <div class="form-group">
+    <form action="{{ action('PostsController@edit')}}" method="POST">
+      {!! csrf_field() !!}
+      <label for="title">Title</label>
+      <input type="text" class="form-control" id="title" name="title" placeholder="{{ old('title') }}">
+      <label for="url">URL</label>
+      <input type="text" class="form-control" id="url" name="url" placeholder="{{ old('url') }}">
+      <label for="content">Content</label>
+      <textarea type="textarea" rows="5" class="form-control" id="content" name="content" placeholder="{{ old('content') }}"></textarea>
+      <br>
+      <button class="btn btn-primary btn-success">Edit Post</button>
+    </form>
+    <br>
+    <form action="{{ action('PostsController@destroy', array(1)) }}" method="POST">
+      {!! csrf_field() !!}
+      <button class="btn btn-primary btn-danger" name="button">Delete Post</button>
+      {!! method_field('DELETE') !!}
+    </form>
+  </div>
+</div>
 @stop
