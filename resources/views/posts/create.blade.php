389eb79ec2838e038307c30479b394d8c440e04c
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-28 15:12:41 -0500

    added alerts to create blade

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,10 +11,13 @@
     <form action="{{ action('PostsController@store')}}" method="POST">
       {!! csrf_field() !!}
       <label for="title">Title</label>
+      {!! $errors->first('title', '<div class="alert alert-danger">:message</div>') !!}
       <input type="text" class="form-control" id="title" name="title" placeholder="Post Title" value="{{ old('title') }}">
       <label for="url">URL</label>
+      {!! $errors->first('url', '<div class="alert alert-danger">:message</div>') !!}
       <input type="text" class="form-control" id="url" name="url" placeholder="Post Link" value="{{ old('url') }}">
       <label for="content">Content</label>
+      {!! $errors->first('content', '<div class="alert alert-danger">:message</div>') !!}
       <textarea type="textarea" rows="5" class="form-control" id="content" name="content" placeholder="Enter content for your post...">{{ old('content') }}</textarea>
       <br>
       <button class="btn btn-primary btn-success">Create Post</button>
