41d96d725d341cb34674efa6e028af1a252fb459
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-28 10:48:08 -0500

    changed action to go to the update method on the posts controller.

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -8,7 +8,7 @@
 <div class="container">
   <h2>Edit Post</h2>
   <div class="form-group">
-    <form action="{{ action('PostsController@edit')}}" method="POST">
+    <form action="{{ action('PostsController@update')}}" method="POST">
       {!! csrf_field() !!}
       <label for="title">Title</label>
       <input type="text" class="form-control" id="title" name="title" placeholder="{{ old('title') }}">
@@ -18,6 +18,7 @@
       <textarea type="textarea" rows="5" class="form-control" id="content" name="content" placeholder="{{ old('content') }}"></textarea>
       <br>
       <button class="btn btn-primary btn-success">Edit Post</button>
+      {!! method_field('PUT') !!}
     </form>
     <br>
     <form action="{{ action('PostsController@destroy', array(1)) }}" method="POST">
