117920adc5e9dff42976b5e13cd5d4fe35c0b07f
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-28 14:37:18 -0500

    updated form action, changed place holders to show the current values of the posts

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -8,20 +8,20 @@
 <div class="container">
   <h2>Edit Post</h2>
   <div class="form-group">
-    <form action="{{ action('PostsController@update')}}" method="POST">
+    <form action="{{ action('PostsController@update', $post->id) }}" method="POST">
       {!! csrf_field() !!}
       <label for="title">Title</label>
-      <input type="text" class="form-control" id="title" name="title" placeholder="{{ old('title') }}">
+      <input type="text" class="form-control" id="title" name="title" placeholder="{{ $post->title }}">
       <label for="url">URL</label>
-      <input type="text" class="form-control" id="url" name="url" placeholder="{{ old('url') }}">
+      <input type="text" class="form-control" id="url" name="url" placeholder="{{ $post->url }}">
       <label for="content">Content</label>
-      <textarea type="textarea" rows="5" class="form-control" id="content" name="content" placeholder="{{ old('content') }}"></textarea>
+      <textarea type="textarea" rows="5" class="form-control" id="content" name="content" placeholder="{{ $post->content }}"></textarea>
       <br>
       <button class="btn btn-primary btn-success">Edit Post</button>
-      {!! method_field('PUT') !!  }
+      {!! method_field('PUT') !!}
     </form>
     <br>
-    <form action="{{ action('PostsController@destroy') }}" method="POST">
+    <form action="{{ action('PostsController@destroy', $post->id) }}" method="POST">
       {!! csrf_field() !!}
       <button class="btn btn-primary btn-danger" name="button">Delete Post</button>
       {!! method_field('DELETE') !!}
