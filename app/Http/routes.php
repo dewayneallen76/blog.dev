1e9b0051c7ac4905250ce2c076102e0a2e219ffd
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-29 09:18:09 -0500

    added laravel provided routes for authentication and registration. authentication lesson

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,10 +13,14 @@
 // Posts routes
 Route::resource('posts', 'PostsController');
 
-// Users routes
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-
-// Votes routes
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 
 // Miscellaneous routes for exercises
