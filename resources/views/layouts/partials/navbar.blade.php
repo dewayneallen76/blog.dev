a0f309ca6c69cb61627d94d24f0066e7e50998a5
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-29 09:59:49 -0500

    added link to navbar for logout

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -14,6 +14,9 @@
       <li class="nav-item">
         <a class="nav-link" href="/auth/register">Register</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ action('Auth\AuthController@getLogout') }}">Logout</a>
+      </li>
     </ul>
   </div>
 </nav>
