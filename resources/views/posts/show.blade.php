7ade94f3d63d866fbeee51bf2a31ed1a765f7321
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-29 15:02:48 -0500

    edited styling on the show blade to show the name of the post created by name

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,12 +11,13 @@
     <div class="row">
       <div class="col">
         <h1>{{ $post->title }}</h1>
-        <h2>{{ $post->url }}</h2>
+        <h4><a href="http://google.com" target="_blank">{{ $post->url }}</a></h4>
       </div>
       <div class="col">
         <br>
-        <h6 class="text-muted">Created: {{ $post->created_at->diffForHumans() }}</h6>
-        <h6 class="text-muted">Updated: {{ $post->updated_at->diffForHumans() }}</h6>
+        <h5 class="text-right">Added by: {{ $post->user->name }}</h5>
+        <h6 class="text-muted text-right">Created: {{ $post->created_at->diffForHumans() }}</h6>
+        <h6 class="text-muted text-right">Updated: {{ $post->updated_at->diffForHumans() }}</h6>
       </div>
     </div>
     <p>{{ $post->content }}</p>
