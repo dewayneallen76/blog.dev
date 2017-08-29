dea0f80ab5eba9430dc572d30a50f1c2a4607fef
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-29 09:32:51 -0500

    added directory in views for auth. added login and register blades. authentication lesson

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title')
+  <title>Login</title>
+@stop
+
+@section('content')
+  <h1>This is where users that are already registered will login.</h1>
+@stop
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title')
+  <title>Register</title>
+@stop
+
+@section('content')
+  <h1>This is the page where new users will register</h1>
+@stop
