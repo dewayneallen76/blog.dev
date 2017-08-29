dbde233cd1ff8b1fc0b6432d453d9cb0a0e62090
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-29 09:59:33 -0500

    added forms for register and login in blades. authentication lesson.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,5 +5,20 @@
 @stop
 
 @section('content')
-  <h1>This is where users that are already registered will login.</h1>
+<div class="container">
+  <h2>Login</h2>
+  <div class="form-group">
+    <form action="{{ action('Auth\AuthController@postLogin') }}" method="POST">
+      {!! csrf_field() !!}
+      <label for="email">Email</label>
+      {!! $errors->first('email', '<div class="alert alert-danger">:message</div>') !!}
+      <input type="text" class="form-control" id="email" name="email" placeholder="Email" value="{{ old('email') }}">
+      <label for="password">Password</label>
+      {!! $errors->first('password', '<div class="alert alert-danger">:message</div>') !!}
+      <input type="password" class="form-control" id="password" name="password" placeholder="Password" value="{{ old('password') }}">
+      <br>
+      <button class="btn btn-primary btn-success">Login</button>
+    </form>
+  </div>
+</div>
 @stop
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,5 +5,26 @@
 @stop
 
 @section('content')
-  <h1>This is the page where new users will register</h1>
+<div class="container">
+  <h2>Register</h2>
+  <div class="form-group">
+    <form action="{{ action('Auth\AuthController@postRegister')}}" method="POST">
+      {!! csrf_field() !!}
+      <label for="name">Name:</label>
+      {!! $errors->first('name', '<div class="alert alert-danger">:message</div>') !!}
+      <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name" value="{{ old('name') }}">
+      <label for="email">Email:</label>
+      {!! $errors->first('email', '<div class="alert alert-danger">:message</div>') !!}
+      <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email address" value="{{ old('email') }}">
+      <label for="password">Password:</label>
+      {!! $errors->first('password', '<div class="alert alert-danger">:message</div>') !!}
+      <input type="password" class="form-control" id="password" name="password" placeholder="Enter a password">
+      <label for="password_confirmation">Password Confirmation:</label>
+      {!! $errors->first('password_confirmation', '<div class="alert alert-danger">:message</div>') !!}
+      <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Confirm Password">
+      <br>
+      <button class="btn btn-primary btn-success">Register</button>
+    </form>
+  </div>
+</div>
 @stop
