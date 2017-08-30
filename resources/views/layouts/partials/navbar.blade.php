4f7262d3ddbd5983a59d6e96b08982aafdfe78ed
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-30 12:29:03 -0500

    Made some changes to the navbar, added actions for register and login, rather than using file paths. Rearranged the view of the navbar.

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,13 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Blog</a>
+  <a class="navbar-brand" href="/">Blog</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="{{ action('PostsController@index') }}">All Posts</a>
-      </li>
       @if(Auth::check())
       <li class="nav-item">
         <a class="nav-link" href="{{ action('UsersController@show', Auth::id()) }}">Welcome {{ Auth::user()->name }}!</a>
@@ -15,16 +12,19 @@
       <li class="nav-item">
         <a class="nav-link" href="{{ action('PostsController@create') }}">Create a Post</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ action('Auth\AuthController@getLogout') }}">Logout</a>
+      </li>
       @else
       <li class="nav-item">
-        <a class="nav-link" href="/auth/login">Login</a>
+        <a class="nav-link" href="{{ action('Auth\AuthController@getLogin') }}">Login</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/auth/register">Register</a>
+        <a class="nav-link" href="{{ action('Auth\AuthController@getRegister') }}">Register</a>
       </li>
       @endif
       <li class="nav-item">
-        <a class="nav-link" href="{{ action('Auth\AuthController@getLogout') }}">Logout</a>
+        <a class="nav-link" href="{{ action('PostsController@index') }}">All Posts</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
