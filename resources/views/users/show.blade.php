bc60f38cd4ac2624184ce52c2c94e742ae9b1b91
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-30 12:20:10 -0500

    Added content to users show blade

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.master')
 
 @section('title')
-  <title>Your Profile</title>
+  <title>User Profile</title>
 @stop
 
 @section('content')
-  <h1>User profile page</h1>
+<br>
+<br>
+<div class="container">
+  <h1 class="text-center">User Profile</h1>
+  <div class="row">
+    <div class="col">
+      <h2>Name: {{ $users->name }}</h2>
+      <h4>Email: {{ $users->email }}</h4>
+    </div>
+    <div class="col">
+      <h4 class="text-right">Member since: {{ $users->created_at->diffForHumans() }}</h4>
+    </div>
+  </div>
+</div>
 @stop
