a97c42da0498a8110cd4597f1153a90b4aedc578
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-30 11:22:06 -0500

    added resource route to routes for the users controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,8 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-// Posts routes
+// Posts routes...
+Route::get('/', 'PostsController@index');
 Route::resource('posts', 'PostsController');
 
 // Authentication routes...
@@ -22,9 +23,9 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 
+// Users routes...
+Route::resource('users', 'UsersController');
 
-// Miscellaneous routes for exercises
-Route::get('/', 'PostsController@index');
 
 Route::get('/sayhello/{name}', 'HomeController@sayHello');
 
