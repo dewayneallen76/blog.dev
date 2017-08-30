a04b864c565253b4efc3c35a5ca16cb4eb252d82
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-30 10:54:57 -0500

    added __construct method for middleware so that not logged in users can only view all posts or individual posts otherwise they are redirected back to the login page.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,6 +14,10 @@ use Log;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+      $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *
