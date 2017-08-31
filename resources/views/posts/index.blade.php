acc67365b5aba0b7629baad475d0e6ac3d309c21
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-31 16:38:45 -0500

    working on getting the pagination to render using search.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,7 +13,7 @@
       <div class="card border-dark mb-3" style="width: 20rem;">
         <img class="card-img-top" src="http://fillmurray.com/318/180" alt="Card image cap">
         <div class="card-body">
-          <h4 class="card-title">{{ $post->title}}</h4>
+          <h4 class="card-title">{{ $post->title }}</h4>
           <p class="card-text">{{ $post->created_at }}</p>
           <p class="card-text" style="max-height: 150px; overflow:hidden;">{{ $post->content }}</p>
           <a class="btn btn-primary" href="{{ action('PostsController@show', $post->id) }}">View Post</a>
@@ -23,7 +23,6 @@
     </div>
     @endforeach
   </div>
-  <div class="text-center">
-  </div>
+
 </div>
 @stop
