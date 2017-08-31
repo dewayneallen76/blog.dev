1540f46e4c010f31f190700f3ac473f37f1ed390
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-31 16:29:02 -0500

    added h1 to test route to user edit blade.

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title')
+  <title>Edit User</title>
+@stop
+
+@section('content')
+<div class="container">
+  <h1>Form for editing user information</h1>
+</div>
+@stop 
