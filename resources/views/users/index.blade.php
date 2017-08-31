bcd0b9839355cf97895fc04d6270227e2bca74ee
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-31 16:08:17 -0500

    added index blade for users

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+
+@section('title')
+  <title>All Users</title>
+@stop
+
+@section('content')
+<div class="container">
+  <h1>All Users</h1>
+  @foreach($users as $user)
+  <div class="row">
+    <div class="col">
+      <p><a href="{{ action('UsersController@show', $user->id) }}">{{ $user->name }}</a></p>
+    </div>
+    <div class="col">
+      <p class="text-right">{{ $user->email }}</p>
+    </div>
+  </div>
+  @endforeach
+</div>
+
+@stop
